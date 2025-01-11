e6eb7df7ab0bfec0f90047eca0171fd818c7dabe
MariaLight <61028743+MariaLight@users.noreply.github.com>
2025-01-11 17:55:52 +1200

    add plugins

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import styles from './app.module.css';
 import { useState, useRef } from "react";
+import * as yup from 'yup';
 
+import { useForm } from 'react-hook-form';
+import { yupResolver } from "@hookform/resolvers/yup";
 
 const sendFormData = (formData) => {
   console.log(formData);
